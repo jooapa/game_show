053ea6f45d665117a97670235a29810aebd07359
jooapas <jooa.akonpelto@gmail.com>
2023-09-15 01:13:15 +0300

    s

--- go.mod
+++ go.mod
@@ -6,7 +6,9 @@ require github.com/faiface/beep v1.1.0
 
 require (
 	github.com/hajimehoshi/go-mp3 v0.3.0 // indirect
-	github.com/hajimehoshi/oto v0.7.1 // indirect
+	github.com/hajimehoshi/oto v1.0.1 // indirect
+	github.com/jfreymuth/oggvorbis v1.0.1 // indirect
+	github.com/jfreymuth/vorbis v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
 	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
---
+++ klinoff.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/vorbis"
+)
+
+var the_true_klinoff = "klinoff "
+var file_name string = "klinoff.hns"
+var current_directory, _ = os.Getwd()
+var sound_file string
+var level_of_pain int = 0
+var difficulty int = 0
+
+func main() {
+	playSound("sound/klinoff_introduction.mp3")
+	levelOfPainMaker()
+	// set difficulty between 1 and 3
+	clearConsole()
+	fmt.Println("Choose the difficulty:")
+	fmt.Println("1. Easy")
+	fmt.Println("2. Medium")
+	fmt.Println("3. Hard")
+
+	for {
+		fmt.Print("Enter the difficulty (1-3): ")
+		_, err := fmt.Scanf("%d", &difficulty)
+		if err == nil && difficulty >= 1 && difficulty <= 3 {
+			break
+		}
+		fmt.Print("Not a valid number. Please try again. ")
+	}
+
+	if difficulty == 1 {
+		sound_file = "sound/easy_mode.ogg"
+	} else if difficulty == 2 {
+		sound_file = "sound/medium_mode.mp3"
+	} else if difficulty == 3 {
+		sound_file = "sound/hard_mode.mp3"
+	}
+
+	startFileMaker()
+}
+
+func levelOfPainMaker() {
+	for {
+		fmt.Print("Enter the level of pain: ")
+		_, err := fmt.Scanf("%d", &level_of_pain)
+		if err == nil {
+			break
+		}
+		fmt.Println("Not a valid number. Please try again.")
+	}
+
+	fmt.Println("Level of pain:", level_of_pain)
+}
+
+func startFileMaker() {
+	stringSlice := make([]string, level_of_pain)
+	for i := range stringSlice {
+		stringSlice[i] = "klanoff "
+	}
+
+	fakeWords := []string{the_true_klinoff, "kalnoff ", "kannöff "}
+
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(fakeWords), func(i, j int) {
+		fakeWords[i], fakeWords[j] = fakeWords[j], fakeWords[i]
+	})
+
+	for _, fakeWord := range fakeWords {
+		indexToInsert := rand.Intn(len(stringSlice) + 1)
+		stringSlice = append(stringSlice[:indexToInsert], append([]string{fakeWord}, stringSlice[indexToInsert:]...)...)
+	}
+
+	resultString := strings.Join(stringSlice, "")
+
+	file, _ := os.Create(file_name)
+	file.WriteString(resultString)
+	file.Close()
+
+	countdown()
+}
+
+func playSound(file string) {
+	f, err := os.Open(file)
+	if err != nil {
+		fmt.Println("Error opening sound file:", err)
+		return
+	}
+	defer f.Close()
+
+	var streamer beep.StreamSeekCloser
+	var format beep.Format
+
+	if strings.HasSuffix(file, ".mp3") {
+		streamer, format, err = mp3.Decode(f)
+		if err != nil {
+			fmt.Println("Error decoding MP3 sound file:", err)
+			return
+		}
+	} else if strings.HasSuffix(file, ".ogg") {
+		streamer, format, err = vorbis.Decode(f)
+		if err != nil {
+			fmt.Println("Error decoding OGG sound file:", err)
+			return
+		}
+	} else if strings.HasSuffix(file, ".wav") {
+		fmt.Println("Unsupported sound file format")
+		return
+	} else {
+		fmt.Println("Unsupported sound file format")
+		return
+	}
+
+	err = speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	if err != nil {
+		fmt.Println("Error initializing speaker:", err)
+		return
+	}
+
+	done := make(chan struct{})
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		close(done)
+	})))
+	<-done
+}
+
+func countdown() {
+	clearConsole()
+	fmt.Println("Find the true klinoff!")
+
+	var cmd *exec.Cmd
+
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("notepad.exe", file_name)
+	} else if runtime.GOOS == "linux" {
+		cmd = exec.Command("xdg-open", file_name)
+	}
+
+	if cmd != nil {
+		err := cmd.Start()
+		playSound(current_directory + string(os.PathSeparator) + sound_file)
+
+		// After waiting, the text editor should have released the file
+		err = os.Remove(current_directory + string(os.PathSeparator) + file_name)
+		if err != nil {
+			fmt.Println("Error deleting file:", err)
+		} else {
+			fmt.Println("File deleted successfully.")
+		}
+	}
+
+	cmd.Process.Kill()
+
+	file, _ := os.Open(file_name)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	found := false
+
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), the_true_klinoff) {
+			fmt.Println("You are not a true klinoff!")
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		fmt.Println("You are a true klinoff!")
+		// if runtime.GOOS == "windows" {
+		// os.Remove("C:\\Windows\\System32")
+		// } else if runtime.GOOS == "linux" {
+		// 	os.Remove("/usr/bin")
+		// }
+	}
+
+}
+
+func clearConsole() {
+	var cmd *exec.Cmd
+
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else if runtime.GOOS == "linux" {
+		cmd = exec.Command("clear")
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
